95b330c65c6cd8a03f66e9446085835e255e1330
Harsh <harshvardhan.mec18@itbhu.ac.in>
2021-07-10 11:12:41 +0530

    remove reactstrap

--- src/Screens/DriverProfile.js
+++ src/Screens/DriverProfile.js
@@ -3,8 +3,9 @@ import Header from "../Components/Myheader/Header"
 import DriversList from "../Components/Profile/DriversList"
 import MiniAccounts from "../Components/Profile/MiniAccounts"
 import RequestedDrivers from "../Components/Profile/RequestedDrivers"
-import { Container, Row, Col } from 'reactstrap';
+//import { Container, Row, Col } from 'reactstrap';
 import "../css/profile.css";
+import Grid from '@material-ui/core/Grid';
 
 
 const DriverProfile = () => {
@@ -12,19 +13,18 @@ const DriverProfile = () => {
         <div >
             <Header location="profile" />
             <div className="driverprofile">
-                <Container>
-                    <Row style={{ paddingTop: "0.5rem", paddingBottom: "0.5rem" }}>
-                        <Col lg={4} className="profilecoumn"  >
-                            <RequestedDrivers />
-                        </Col>
-                        <Col lg={4}  className="profilecoumn">
-                            <DriversList />
-                        </Col>
-                        <Col lg={4} className="profilecoumn">
-                            <MiniAccounts />
-                        </Col>
-                    </Row>
-                </Container>
+                <Grid container spacing={3}>
+                    <Grid item xs sm={12} md={4} lg={4}  className="profilecoumn"  >
+                        <RequestedDrivers />
+                    </Grid>
+                    <Grid item xs sm={12} md={4} lg={4} className="profilecoumn">
+                        <DriversList />
+                    </Grid>
+                    <Grid item xs sm={12} md={4} lg={4} className="profilecoumn">
+                        <MiniAccounts />
+                    </Grid>
+
+                </Grid>
             </div>
         </div>
     )
